4f7f45962f16014a7bbac4041a6fc738a35d929e
Jerry <26147214+jerwng@users.noreply.github.com>
2021-06-15 18:34:01 -0400

    Adds navbar and input component files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+
+import TopNavbar from "./navbar.js"
+import Inputs from "./inputs.js"
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <TopNavbar />
+      <Inputs />
     </div>
   );
 }
---
+++ src/navbar.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import Navbar from 'react-bootstrap/Navbar';
+
+function TopNavbar() {
+  return <Navbar bg="dark" variant="dark">
+    <Navbar.Brand href="#home">Image PDF Searcher</Navbar.Brand>
+  </Navbar>;
+}
+
+export default TopNavbar;
