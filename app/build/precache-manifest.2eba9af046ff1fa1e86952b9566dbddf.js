e16533b9de633718ffcbaec151bb5d9f460ed7ac
Jerry <26147214+jerwng@users.noreply.github.com>
2021-09-20 18:23:50 -0400

    add build

---
+++ app/build/precache-manifest.2eba9af046ff1fa1e86952b9566dbddf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "3b250c7de3b68cfcd484",
+    "url": "/static/js/runtime~main.81759bae.js"
+  },
+  {
+    "revision": "3008748969eeed54eb9f",
+    "url": "/static/js/main.511edac7.chunk.js"
+  },
+  {
+    "revision": "59932cb71e3d349fd23d",
+    "url": "/static/js/3.e2279214.chunk.js"
+  },
+  {
+    "revision": "54656c326543bab9c4af",
+    "url": "/static/js/2.627ab75c.chunk.js"
+  },
+  {
+    "revision": "3008748969eeed54eb9f",
+    "url": "/static/css/main.aef91499.chunk.css"
+  },
+  {
+    "revision": "0f2278428ddc831ac34fad6de958e61e",
+    "url": "/index.html"
+  }
+];
