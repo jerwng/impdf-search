ccea45c6462de1578ba5397d8390db4e0afe3637
Jerry Wang <26147214+jerwng@users.noreply.github.com>
2021-10-17 21:35:29 -0400

    Add documentation for images

--- app/src/images.js
+++ app/src/images.js
@@ -5,6 +5,15 @@ import Col from 'react-bootstrap/Col'
 
 import './css/images.css';
 
+/**
+ * The images component to display the rows of thumbnail sized images.
+ * 
+ * Sub-component for: App
+ * 
+ * @param {String} props.photos The photos converted from the PDF. data:image/jpeg;base64 format.
+ * @param {Function} props.handleClick Function for when a image is clicked.
+ * @returns The images component to display the rows of thumbnail sized images.
+ */
 function Images(props) {
   return (
     <Row xs="auto"> {
@@ -24,6 +33,13 @@ function Images(props) {
   )
 }
 
+/**
+ * Sub-component for: Images
+ * 
+ * @param {String} props.photos The individual photo converted from the PDF. data:image/jpeg;base64 format.
+ * @param {Number} props.id The index of the indivual photo.
+ * @returns An <img> element for the individual photo.
+ */
 function Thumbnail(props) {
   const handleClick = () => {
     props.handleClick(props.id)
