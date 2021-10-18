5483095ef1b7060ab984faf1f026f52f767576f8
Jerry Wang <26147214+jerwng@users.noreply.github.com>
2021-10-18 00:19:00 -0400

    Test build, migrate to Heroku-Redis, modify redis url

--- worker.py
+++ worker.py
@@ -4,9 +4,9 @@ import redis
 from rq import Worker, Queue, Connection
 
 listen = ['default']
-redis_url = os.getenv('REDISTOGO_URL', 'redis://localhost:6379')
+# redis_url = os.getenv('REDISTOGO_URL', 'redis://localhost:6379')
 
-conn = redis.from_url(redis_url)
+conn = redis.from_url(os.environ.get("REDIS_URL"))
 
 if __name__ == "__main__":
     with Connection(conn):
