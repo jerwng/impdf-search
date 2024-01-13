a7c023c9f106ea7ef0e0d60b2a97408f6b9e9a09
jerry <chenyuan.wang@mail.utoronto.ca>
2024-01-12 23:29:42 -0500

    Add OBJC_DISABLE_INITIALIZE_FORK_SAFETY enviroment variable to bypass rq forking issues

--- worker.py
+++ worker.py
@@ -3,6 +3,11 @@ import redis
 
 from rq import Worker, Queue, Connection
 
+# This is to disable fork issue with rq and macOS
+# Bypass this check to prevent enqueue calls from being blocked
+# https://github.com/rq/rq/issues/1418
+os.environ['OBJC_DISABLE_INITIALIZE_FORK_SAFETY'] = 'YES'
+
 listen = ['default']
 
 conn = redis.from_url(os.environ.get("REDIS_URL"))
