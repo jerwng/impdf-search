c1d3a699153b6d41e4ab6976fce19a3204777867
jerry <chenyuan.wang@mail.utoronto.ca>
2024-01-15 21:50:36 -0500

    Update folder structure to separate frontend and backend

--- heroku.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-gunicorn server:app --daemon
-python worker.py
--- ocr/test-pdf2img.py
+++
@@ -1,6 +0,0 @@
-from pdf2image import convert_from_path
-
-pages = convert_from_path('A32NX.pdf')
-
-for page in pages:
-    page.save('out.jpg', 'JPEG')
